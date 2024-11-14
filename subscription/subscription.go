3df3b64215e37e897606c63fedfbc64f27210ea6
Ed Costello <edc.open@gmail.com>
2024-11-14 21:32:25 +1300

    Save jetstream cursor

--- subscription/subscription.go
+++ subscription/subscription.go
@@ -46,6 +46,7 @@ func SubscribeJetstream(ctx context.Context, serverAddr string, database *databa
 		Level:     slog.LevelInfo,
 		AddSource: true,
 	})))
+	logger := slog.Default()
 
 	config := client.DefaultClientConfig()
 	config.WebsocketURL = serverAddr
@@ -56,11 +57,47 @@ func SubscribeJetstream(ctx context.Context, serverAddr string, database *databa
 	}
 	
 
-	logger := slog.Default()
+	eventCountSinceSync := 0
+	windowStart := time.Now().UTC().UnixMilli()
+	var lastEvtTime int64 = 0
 	scheduler := js_sequentiial.NewScheduler("feed_generator", logger, func (ctx context.Context, event *models.Event) error {
 		if event.Commit != nil {
 			listener := listeners[event.Commit.Collection]
-			return listener(ctx, event, fmt.Sprintf("at://%s/%s/%s", event.Did, event.Commit.Collection, event.Commit.RKey))
+			err := listener(ctx, event, fmt.Sprintf("at://%s/%s/%s", event.Did, event.Commit.Collection, event.Commit.RKey))
+			if err != nil {
+				return err
+			}
+
+			eventCountSinceSync++
+			if eventCountSinceSync >= 1000 {
+				database.Updates.SaveCursor(ctx, writeSchema.SaveCursorParams{
+					Service: "jetstream",
+					Cursor:  event.TimeUS,
+				})
+				windowEnd := time.Now().UTC().UnixMilli()
+				timeSpent := windowEnd - windowStart
+				parsedTime := time.UnixMicro(event.TimeUS)
+				evtTime := parsedTime.UnixMilli()
+				caughtUp := evtTime - lastEvtTime
+				lagTime := windowEnd - evtTime
+				toCatchUp := time.Duration(0)
+				if lagTime > caughtUp {
+					toCatchUp = time.Duration(timeSpent*lagTime/caughtUp) * time.Millisecond
+				}
+				fmt.Printf(
+					"Processed %d events in %s (%f evts/s), %s caughtUp %s, %s behind, %s to catch up)\n",
+					eventCountSinceSync,
+					time.Duration(timeSpent)*time.Millisecond,
+					1000.0*float64(eventCountSinceSync)/float64(timeSpent),
+					parsedTime.Format(time.RFC3339),
+					time.Duration(caughtUp)*time.Millisecond,
+					time.Duration(lagTime)*time.Millisecond,
+					toCatchUp,
+				)
+				windowStart = windowEnd
+				lastEvtTime = evtTime
+				eventCountSinceSync = 0
+			}
 		}
 	
 		return nil
@@ -71,7 +108,16 @@ func SubscribeJetstream(ctx context.Context, serverAddr string, database *databa
 		log.Fatalf("failed to create client: %v", err)
 	}
 
+	cursorResult, err := database.Queries.GetCursor(ctx, "jetstream")
 	cursor := time.Now().Add(5 * -time.Minute).UnixMicro()
+	if err != nil {
+		return fmt.Errorf("unable to load cursor: %w", err)
+	}
+	if len(cursorResult) > 0 {
+		cursor = cursorResult[0].Cursor
+		fmt.Printf("Cursor from db %d\n", cursor)
+	}
+
 
 	if err := client.ConnectAndRead(ctx, &cursor); err != nil {
 		log.Fatalf("failed to connect: %v", err)
